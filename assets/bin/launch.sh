238747ea777be5f31c291760c0711ba8600f5bfe
Cédric Vanet <dev@cedvan.com>
2015-02-03 00:38:23 +0100

    Fix SATIS_INIT

--- assets/bin/launch.sh
+++ assets/bin/launch.sh
@@ -3,16 +3,16 @@
 WORK_DIRECTORY=/var/www
 SATIS_DIRECTORY=/opt/satis
 BIN_DIRECTORY=/bin/satis
-
-SATIS_INIT=${SATIS_INIT:-false}
 SATIS_HTTPS=${SATIS_HTTPS:-false}
 
+source $BIN_DIRECTORY/config.sh
+
 # Initilisation
-if [ "${SATIS_INIT}" == "false" ]; then
+if [ "$SATIS_INIT" == "false" ]; then
     source $BIN_DIRECTORY/nginx.sh
     source $BIN_DIRECTORY/composer.sh
-    mkdir /var/www/init
-    export SATIS_INIT="true"
+    sed -i "s/SATIS_INIT=false/SATIS_INIT=true/g" $BIN_DIRECTORY/config.sh
 fi
 
+# Load Packages
 source $BIN_DIRECTORY/build.sh
